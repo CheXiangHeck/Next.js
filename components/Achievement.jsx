d3ba61dcb2306d6d4852d1933619dd2abd1996a8
Wei Xiang <chehi716@gmail.com>
2023-12-10 14:28:47 +0800

    Add.

--- components/Achievement.jsx
+++ components/Achievement.jsx
@@ -21,10 +21,9 @@ const Achievement = () => {
             <div className="bg-gradient-to-b from-black to-white p-6 h-[100px] w-full">
             </div>
             <div className="h-full w-full flex justify-center items-center">
-                {achievements.map((achievement, index) => {
+                {achievements.map((achievement) => {
                     return (
                         <div
-                            key={index}  {/* Corrected comment syntax */}
                             className="flex flex-col justify-center items-center w-[200px] p-5"
                         >
                             <h2 className="text-emerald-400 font-bold text-4xl">
